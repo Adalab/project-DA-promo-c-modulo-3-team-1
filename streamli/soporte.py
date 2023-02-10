627f88e89e38b8aa05a2ddb5fb9277a17ceff38b
Anadalab <ana.campos.cordobes@gmail.com>
2023-02-10 13:41:15 +0100

    librerias-soporte

--- streamli/soporte.py
+++ streamli/soporte.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
